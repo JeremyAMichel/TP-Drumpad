ac7874f81d90b68d763bcc79d835b4f04c5e9d61
JeremyAMichel <jeremy.a.michel@hotmail.fr>
2023-06-27 11:49:12 +0200

    correction

--- main.js
+++ main.js
@@ -2,12 +2,23 @@ let keys = document.querySelectorAll(".key");
 let audio;
 let key;
 
+window.addEventListener("keydown", playSound);
+// window.addEventListener("keydown", (event) => {
+//  console.log(event);
+// });
+
+
+keys.forEach(function (key) {
+  key.addEventListener("transitionend", removeTransition);
+});
+
 function playSound(event) {
-  console.log(event.keycode);
+  // console.log(event);
   audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
   key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
 
   if (!audio) return;
+
   audio.currentTime = 0;
   audio.play();
   key.classList.add("playing");
@@ -17,12 +28,9 @@ function removeTransition(event) {
   event.target.classList.remove("playing");
 }
 
-window.addEventListener("keydown", playSound);
-keys.forEach(function (key) {
-  key.addEventListener("transitionend", removeTransition);
-});
 
-/*##########################################*/
+
+// /*##########################################*/
 
 async function beatBox() {
   function simulateKey(keyCode) {
@@ -40,30 +48,38 @@ async function beatBox() {
     });
   }
 
-  await playBeat(65, 600);
-  await playBeat(90, 200);
-  await playBeat(88, 400);
 
-  await playBeat(81, 700);
-  await playBeat(68, 900);
-  await playBeat(67, 1000);
-
-  await playBeat(87, 1200);
-  await playBeat(83, 1400);
-  await playBeat(69, 1600);
+  while(loop){
+    await playBeat(65, 100);
+    await playBeat(90, 200);
+    await playBeat(88, 250);
+  
+    await playBeat(81, 300);
+    await playBeat(68, 400);
+    await playBeat(67, 450);
+  
+    await playBeat(87, 500);
+    await playBeat(83, 550);
+    await playBeat(69, 600);
+  }
+  
 }
 
+let loop;
 document.addEventListener("keydown", (event) => {
-  let loop = true;
-  if (event.keyCode === 71) {
-    // Vérifie si la touche appuyée est la touche G (keyCode 71)
-    document.addEventListener("keydown", (event) => {
-      if (event.keyCode === 72){
-        loop = false;
-      }    
-    });
-    while (loop) {
+   loop = true;
+   // Vérifie si la touche appuyée est la touche G (keyCode 71)
+  if (event.keyCode === 71) {    
       beatBox(); // Lance la séquence de "beats"
-    }
   }
 });
+
+document.addEventListener("keydown", (event) => {
+  // Vérifie si la touche appuyée est la touche H (keyCode 72)
+  if (event.keyCode === 72){
+    loop = false;
+  }    
+});
+
+
+
